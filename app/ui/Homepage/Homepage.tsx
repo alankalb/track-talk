9362f511af2c191419a116c55dbe00e8b0348ac9
Alan Kalbfleisch <alan.kalbfleisch@shopify.com>
2020-05-24 18:07:17 -0400

    test query

--- app/ui/Homepage/Homepage.tsx
+++ app/ui/Homepage/Homepage.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { useQuery } from "react-apollo";
+import PostQuery, { PostsQueryData } from "./graphql/PostsQuery.graphql";
 
 export default function Homepage() {
+  const { loading, error, data } = useQuery<PostsQueryData>(PostQuery);
+
+  console.log(data);
+  console.log(loading);
+  console.log(error);
+
   return <p>Hello World!!</p>;
 }
