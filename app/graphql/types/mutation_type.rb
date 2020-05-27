2bb08fdf83aee1c26a0a91280bcae9c7696d64ac
Alan Kalbfleisch <alan.kalbfleisch@shopify.com>
2020-05-26 20:36:40 -0400

    Create User mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < BaseObject
     field :create_post, mutation: Mutations::CreatePost
     field :create_comment, mutation: Mutations::CreateComment
+    field :create_user, mutation: Mutations::CreateUser
   end
 end
