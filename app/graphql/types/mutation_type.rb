cd6a2677aab22a40c344a7c5b9c234632298ccb0
Alan Kalbfleisch <alan.kalbfleisch@shopify.com>
2020-05-26 20:55:24 -0400

    signin user

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@ module Types
     field :create_post, mutation: Mutations::CreatePost
     field :create_comment, mutation: Mutations::CreateComment
     field :create_user, mutation: Mutations::CreateUser
+    field :signin_user, mutation: Mutations::SignInUser
   end
 end
