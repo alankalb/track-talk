11950bf01b38abd19ed6247895cf367e75596dd4
Alan Kalbfleisch <alan.kalbfleisch@shopify.com>
2020-06-22 21:32:08 -0400

    Connection front end testing

--- app/javascript/components/Homepage/graphql/PostsQuery.graphql.d.ts
+++ app/javascript/components/Homepage/graphql/PostsQuery.graphql.d.ts
@@ -1,35 +1,53 @@
 import { DocumentNode } from "graphql-typed";
 export namespace PostsQueryPartialData {
-  export interface PostsComments {
-    __typename?: "Comment" | null;
-    text?: string | null;
+  export interface PostsEdgesNodePostedBy {
+    __typename?: "User" | null;
+    name?: string | null;
+    id?: string | null;
   }
-  export interface Posts {
+  export interface PostsEdgesNode {
     __typename?: "Post" | null;
     id?: number | null;
     text?: string | null;
     title?: string | null;
-    comments?: (PostsQueryPartialData.PostsComments | null)[] | null;
+    postedBy?: PostsQueryPartialData.PostsEdgesNodePostedBy | null;
+  }
+  export interface PostsEdges {
+    __typename?: "PostEdge" | null;
+    node?: PostsQueryPartialData.PostsEdgesNode | null;
+  }
+  export interface Posts {
+    __typename?: "PostConnection" | null;
+    edges?: (PostsQueryPartialData.PostsEdges | null)[] | null;
   }
 }
 export interface PostsQueryPartialData {
-  posts?: (PostsQueryPartialData.Posts | null)[] | null;
+  posts?: PostsQueryPartialData.Posts | null;
 }
 export namespace PostsQueryData {
-  export interface PostsComments {
-    __typename: "Comment";
-    text: string;
+  export interface PostsEdgesNodePostedBy {
+    __typename: "User";
+    name: string;
+    id: string;
   }
-  export interface Posts {
+  export interface PostsEdgesNode {
     __typename: "Post";
     id: number;
     text: string;
     title: string;
-    comments: PostsQueryData.PostsComments[];
+    postedBy?: PostsQueryData.PostsEdgesNodePostedBy | null;
+  }
+  export interface PostsEdges {
+    __typename: "PostEdge";
+    node?: PostsQueryData.PostsEdgesNode | null;
+  }
+  export interface Posts {
+    __typename: "PostConnection";
+    edges?: (PostsQueryData.PostsEdges | null)[] | null;
   }
 }
 export interface PostsQueryData {
-  posts: PostsQueryData.Posts[];
+  posts: PostsQueryData.Posts;
 }
 declare const document: DocumentNode<PostsQueryData, never, PostsQueryPartialData>;
 export default document;
